ea9b94bad94f6aa317eb3cacbad784f8d991b9ae
heerim hong <devheerim@gmail.com>
2024-08-15 14:06:14 +0900

    Chore: add git ignore

--- 자바스크립트/Object/object-likes-string.js
+++ 자바스크립트/Object/object-likes-string.js
@@ -2,6 +2,7 @@
  * object는 string을 좋아한다.
  * 1. Array의 index도 string으로 처리한다.
  * 2. key로 undefined, null도 가능하며, string으로 처리한다.
+ * 3. object랑 유사한 map은 아니다. 숫자 1과 스트링 1을 구분한다.
  */
 
 // 1.
@@ -41,3 +42,13 @@ const obj6 = {
   [key2]: "possible3?",
 };
 console.log(obj6.null);
+
+// 3.
+const map = new Map([
+  [1, "one"],
+  [2, "two"],
+  [3, "three"],
+]);
+
+console.log(map.get(1)); // "one"
+console.log(map.get("1")); // undefined
