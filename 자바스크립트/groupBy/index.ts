46bef08669e8ec1b45e7c5b877d230f1fda4732d
heerim hong <devheerim@gmail.com>
2025-01-14 13:42:25 +0900

    [자바스크립트] : groupBy

---
+++ 자바스크립트/groupBy/index.ts
@@ -0,0 +1,22 @@
+const array = [
+  { name: "asparagus", type: "vegetables", quantity: 5 },
+  { name: "bananas", type: "fruit", quantity: 0 },
+  { name: "goat", type: "meat", quantity: 23 },
+  { name: "cherries", type: "fruit", quantity: 5 },
+  { name: "fish", type: "meat", quantity: 22 },
+];
+
+const groupBy = <T>(iterables: T[], callback: (iter: T) => string) => {
+  const map = new Map<ReturnType<typeof callback>, T[]>();
+  for (const iter of iterables) {
+    const newKey = callback(iter);
+    if (!map.has(newKey)) {
+      map.set(newKey, []);
+    }
+    map.get(newKey)!.push(iter);
+  }
+  return Object.fromEntries(map); //  return map; 처럼 그냥 사용해도 괜찮을듯.
+};
+
+const result1 = groupBy(array, (iter) => iter.type);
+console.log(result1);
